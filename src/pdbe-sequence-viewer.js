b7fd4cae7afc68cdbabc86ce78ff7b6e3374fa55
Sreenath Sasidharan Nair <33874447+sreenathnair@users.noreply.github.com>
2018-01-09 12:18:28 +0000

    Add files via upload

--- src/pdbe-sequence-viewer.js
+++ src/pdbe-sequence-viewer.js
@@ -88,31 +88,31 @@ class PDBeSequenceViewer extends HTMLElement {
                     {
                       accession: "modified", locations: [{
                         fragments: []
-                      }] , color: config["color_code"]["modified"]
+                      }], color: config["color_code"]["modified"]
                     }
                   ];
 
                   // populating molecule fragment
-                  for(let incr = compObj._displaystart; incr <= compObj._displayend; incr++) {
+                  for (let incr = compObj._displaystart; incr <= compObj._displayend; incr++) {
                     var tempFragment = {
                       start: incr,
                       end: incr,
-                      toolTip: "Residue " +incr +" (" +compObj._pdbSequence.charAt(incr) +") <br>" +
-                        "<b>" +compObj._pdbid +"</b>"
+                      toolTip: "Residue " + incr + " (" + compObj._pdbSequence.charAt(incr) + ") <br>" +
+                        "<b>" + compObj._pdbid + "</b>"
                     }
                     moleculeData[0].locations[0].fragments.push(tempFragment);
                   }
-                  
+
                   // populating mutated residue fragment
-                  if(combinedResult[compObj._pdbid]["mutatedResidues"]["resolve"]) {
+                  if (combinedResult[compObj._pdbid]["mutatedResidues"]["resolve"]) {
                     combinedResult[compObj._pdbid]["mutatedResidues"]["result"][compObj._pdbid].forEach(mutatedElement => {
-                      
-                      if(mutatedElement.entity_id && mutatedElement.entity_id == 1 && mutatedElement.chain_id == compObj._bestChainId) {
+
+                      if (mutatedElement.entity_id && mutatedElement.entity_id == 1 && mutatedElement.chain_id == compObj._bestChainId) {
                         let tempFragment = {
                           start: mutatedElement.residue_number,
                           end: mutatedElement.residue_number,
-                          toolTip: mutatedElement.mutation_details.from +" --> " +mutatedElement.mutation_details.to +
-                            " (" +mutatedElement.mutation_details.type +")"
+                          toolTip: mutatedElement.mutation_details.from + " --> " + mutatedElement.mutation_details.to +
+                            " (" + mutatedElement.mutation_details.type + ")"
                         }
                         moleculeData[1].locations[0].fragments.push(tempFragment);
                       }
@@ -121,14 +121,14 @@ class PDBeSequenceViewer extends HTMLElement {
                   }
 
                   // populating modified residue fragment
-                  if(combinedResult[compObj._pdbid]["modifiedResidues"]["resolve"]) {
+                  if (combinedResult[compObj._pdbid]["modifiedResidues"]["resolve"]) {
                     combinedResult[compObj._pdbid]["modifiedResidues"]["result"][compObj._pdbid].forEach(modifiedElement => {
-                      
-                      if(modifiedElement.entity_id && modifiedElement.entity_id == 1 && modifiedElement.chain_id == compObj._bestChainId) {
+
+                      if (modifiedElement.entity_id && modifiedElement.entity_id == 1 && modifiedElement.chain_id == compObj._bestChainId) {
                         let tempFragment = {
                           start: modifiedElement.residue_number,
                           end: modifiedElement.residue_number,
-                          toolTip: "Modified Residue: " +modifiedElement.chem_comp_id
+                          toolTip: "Modified Residue: " + modifiedElement.chem_comp_id
                         }
                         moleculeData[2].locations[0].fragments.push(tempFragment);
                       }
@@ -139,46 +139,49 @@ class PDBeSequenceViewer extends HTMLElement {
                   document.querySelector('#molecule-track').data = moleculeData;
 
                   // painting mapping components
-                  
-                  if(combinedResult[compObj._pdbid]["mappings"]["resolve"]) {
+
+                  if (combinedResult[compObj._pdbid]["mappings"]["resolve"]) {
 
                     compObj.mappingsDiv = compObj.mainContent.append('div')
                       .attr('id', 'mappings-div');
-                    
+
                     compObj.mappingsDiv.append('div')
-                      .attr('class', 'left')
-                      .text('Mappings');
-                    
+                      .attr('class', 'left category')
+                      .text('Mappings')
+                      .on('click', function() {
+                        compObj.toggle($(this));
+                      });
+
                     compObj.mappingsDiv.append('protvista-track')
                       .attr('id', 'mappings-summary-track')
                       .attr('class', 'right')
+                      .style('display', 'inline-block')
                       .attr('length', compObj._pdbSequenceLength)
                       .attr('displaystart', compObj._displaystart)
                       .attr('displayend', compObj._displayend);
 
                     document.querySelector('#mappings-summary-track').data = moleculeData;
-                      
+
                     let mappingsTrack = compObj.mappingsDiv.append('div')
-                      .attr('id', 'mappings-tracks');
-                    
+                      .attr('id', 'mappings-tracks')
+                      .style('display', 'none');
+
                     let mappingResults = combinedResult[compObj._pdbid]["mappings"]["result"][compObj._pdbid];
                     let mappingsToProcess = [];
 
                     Object.keys(mappingResults).forEach(key => {
-                      componentHash[key.toLowerCase()] != undefined ? mappingsToProcess.push(key):null;
+                      componentHash[key.toLowerCase()] != undefined ? mappingsToProcess.push(key) : null;
                     });
 
-                    console.log(mappingsToProcess)
-
                     mappingsToProcess.forEach(mapping => {
-                      
+
                       mappingsTrack.append('div')
                         .attr('class', 'left category-header')
                         .text(mapping);
-                      
+
                       mappingsTrack.append('protvista-track')
                         .attr('class', 'right')
-                        .attr('id', mapping +'-track')
+                        .attr('id', mapping + '-track')
                         .attr('length', compObj._pdbSequenceLength)
                         .attr('displaystart', compObj._displaystart)
                         .attr('displayend', compObj._displayend);
@@ -189,42 +192,143 @@ class PDBeSequenceViewer extends HTMLElement {
                             fragments: []
                           }], color: config["color_code"][mapping.toLowerCase()]
                         }];
-                      
+
 
                       Object.keys(mappingResults[mapping]).forEach(result => {
 
                         mappingResults[mapping][result]['mappings'].forEach(elementMapping => {
-                          
 
-                          for(let incr = elementMapping.start.residue_number; incr <= elementMapping.end.residue_number; incr++) {
+
+                          for (let incr = elementMapping.start.residue_number; incr <= elementMapping.end.residue_number; incr++) {
 
                             let uniprotTooltip = "";
+
                             // add UniProt details to tool tip if applicable
-                            if(mapping === 'UniProt') {
-                              uniprotTooltip = "UniProt range: " +elementMapping.unp_start +" - " +elementMapping.unp_end +"<br>";
+
+                            if (mapping === 'UniProt') {
+                              uniprotTooltip = "UniProt range: " + elementMapping.unp_start + " - " + elementMapping.unp_end + "<br>";
                             }
 
                             let fragment = {
                               start: incr,
                               end: incr,
-                              toolTip: "Residue " +incr +" (" +compObj._pdbSequence.charAt(incr) +")" +
-                                "<br><b>" +result +"</b><br>" +
-                                mappingResults[mapping][result].identifier +"<br>" +
+                              toolTip: "Residue " + incr + " (" + compObj._pdbSequence.charAt(incr) + ")" +
+                                "<br><b>" + result + "</b><br>" +
+                                mappingResults[mapping][result].identifier + "<br>" +
                                 uniprotTooltip +
-                                "PDB range: " +elementMapping.start.residue_number +" - " +
-                                elementMapping.end.residue_number +" (Chain " +compObj._bestChainId +")"
+                                "PDB range: " + elementMapping.start.residue_number + " - " +
+                                elementMapping.end.residue_number + " (Chain " + compObj._bestChainId + ")"
                             }
-                            
+
                             mappingData[0].locations[0].fragments.push(fragment);
                           }
                         });
                       });
 
-                      document.querySelector("#" +mapping +"-track").data = mappingData;
+                      document.querySelector("#" + mapping + "-track").data = mappingData;
 
                     });
 
                   }
+
+                  // painting structural features
+
+                  if(combinedResult[compObj._pdbid]["secStrutures"]["resolve"]) {
+                    let secStructureResults = combinedResult[compObj._pdbid]["secStrutures"]["result"][compObj._pdbid]["molecules"];
+
+                    compObj.secStructuresDiv = compObj.mainContent.append('div')
+                      .attr('id', 'sec-structures-div');
+
+                    compObj.secStructuresDiv.append('div')
+                      .attr('class', 'left category')
+                      .text('Structural Features')
+                      .on('click', function() {
+                        compObj.toggle($(this));
+                      });
+
+                    compObj.secStructuresDiv.append('div')
+                      .attr('id', 'sec-structures-summary')
+                      .attr('class', 'right')
+                        .append('protvista-track')
+                        .attr('id', 'sec-structures-summary-track')
+                        .attr('length', compObj._pdbSequenceLength)
+                        .attr('displaystart', compObj._displaystart)
+                        .attr('displayend', compObj._displayend);
+
+                    compObj.secStructuresTracks =  compObj.secStructuresDiv.append('div')
+                      .attr('id', 'sec-structures-tracks')
+                      .attr('style', 'display:none');
+
+                    secStructureResults.forEach(secStructureElement => {
+
+                      if(secStructureElement.entity_id == 1) {
+
+                        secStructureElement["chains"].forEach(chain => {
+
+                          if(chain.chain_id == compObj._bestChainId) {
+
+                            let secStructureData = chain["secondary_structure"];
+                            let secStructureSummaryData = [];
+
+                            let secStructuresToProcess = [];
+
+                            // creating list of secondary structures to paint
+                            config["category"][0]["structural_features"]["features"].forEach(feature => {
+                              if(compObj._components.toLowerCase().indexOf(feature) != -1) {
+                                secStructuresToProcess.push(feature);
+                              }
+                            });
+
+                            let secStructureKey = config["category"][0]["structural_features"]["keys"];
+
+                            secStructuresToProcess.forEach(feature => {
+                              //console.log(feature)
+                              let data = [{
+                                accession: feature, locations: [{
+                                  fragments: []
+                                }], color: config["color_code"][feature]
+                              }];
+
+                              secStructureData[secStructureKey[feature]].forEach(structure => {
+                                //console.log(structure);
+
+                                let fragment = {
+                                  start: structure.start.residue_number,
+                                  end: structure.end.residue_number,
+                                  toolTip: "A " +feature +" in Chain " +compObj._bestChainId
+                                };
+
+                                data[0].locations[0].fragments.push(fragment);
+                                
+                              });
+
+                              secStructureSummaryData.push(data[0]);
+
+                              compObj.secStructuresTracks.append('div')
+                                .attr('class', 'left category-header')
+                                .text(compObj.toCamelCase(secStructureKey[feature]));
+
+                              compObj.secStructuresTracks.append('protvista-track')
+                                .attr('id', feature +"-track")
+                                .attr('class', 'right')
+                                .attr('length', compObj._pdbSequenceLength)
+                                .attr('displaystart', compObj._displaystart)
+                                .attr('displayend', compObj._displayend);
+
+                              document.querySelector("#" +feature +"-track").data = data;
+
+                            });
+
+                            document.querySelector('#sec-structures-summary-track').data = secStructureSummaryData;
+
+                          }
+
+                        });
+                      }
+                    });
+
+                  }
+
 
                 }
 
@@ -299,7 +403,7 @@ class PDBeSequenceViewer extends HTMLElement {
       .attr('id', 'molecule-div');
 
     this.moleculeDiv.append('div')
-      .attr('class', 'left')
+      .attr('class', 'left category')
       .text('Molecule');
 
     this.moleculeDiv.append('protvista-track')
@@ -320,6 +424,12 @@ class PDBeSequenceViewer extends HTMLElement {
   flattenArray(arrArg) {
     return arrArg.reduce((a, b) => a.concat(b));
   }
+
+  toCamelCase (str) {
+    
+    return str.split('').reduce((t, v, k) => t + (k === 0 ? v.toUpperCase() : v), '');
+  }
+  
 
   // this function binds change events to all protvista components
   bindEvents() {
@@ -345,6 +455,20 @@ class PDBeSequenceViewer extends HTMLElement {
         }
       });
     });
+  }
+
+  toggle(obj) {
+
+    var tracksContainer = obj.parent().children().last();
+    var summaryContainer = obj.parent().children().first().next();
+
+    if ("none" === tracksContainer.css("display")) {
+      tracksContainer.css("display", "inline-block");
+      summaryContainer.css("display", "none");
+    } else {
+      tracksContainer.css("display", "none");
+      summaryContainer.css("display", "inline-block");
+    }
   }
 
 }
